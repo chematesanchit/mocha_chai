52ad4aaac7174e6846370a65412058bc4f5b13cf
sanchit <sanchitchemate@gmail.com>
2024-01-02 00:02:02 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+ app.use('/user', require('./routes/user')); // Mount the users router under /user
+
+
+app.listen(port, () => {
+    console.log(`App is listening at http://localhost:${port}`);
+});
